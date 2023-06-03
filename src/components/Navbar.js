7c32e9cb9cd6b6343632d5d8a6216df4bc2842d0
naimatullahRezaee <naimatullahrezaee@gmail.com>
2023-06-03 07:11:27 +0200

    function for color changes created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import SearchBar from "./SearchBar";
+
+import { useTheme } from "../hooks/UseTheme";
 export default function Navbar() {
+  const { color, changeColor } = useTheme();
   return (
-    <div className="navbar">
-      <nav>
+    <div className="navbar" style={{ background: color }}>
+      <nav onClick={() => changeColor("green")}>
         <Link to="/" className="brand">
           <h1>Recipe Food</h1>
         </Link>
